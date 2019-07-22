d602fcf14e1aced0cfe70355cfa8654a3102e533
anferoar96 <afromana@unal.edu.co>
2019-07-21 20:54:49 -0500

    Probando 4

--- Andres/Cuda/matrix.cu
+++ Andres/Cuda/matrix.cu
@@ -23,7 +23,6 @@ __global__ void multiplication(int*  a,int* b,int*  c,int n){
 
     c[row*n+col]=partial;
 }
-
 void  multiplication2(int* a,int* b,int* c,int  size){
     for(int i=0;i<size;i++){
         for(int   j=0;j<size;i++){
@@ -37,14 +36,13 @@ void  multiplication2(int* a,int* b,int* c,int  size){
 
 int main(int argc, char **argv)
 {
-    printf("Inicio");
+    
     //define variables
     int  n  = 1024;
     //Host matrix
     int* h_a;
     int* h_b;
     int* h_c;
-    int* h_c_s;
 
     //Device  matrix
     int* d_a;
@@ -57,7 +55,6 @@ int main(int argc, char **argv)
     h_a =(int*)malloc(bytes);
     h_b =(int*)malloc(bytes);
     h_c =(int*)malloc(bytes);
-    h_c_s =(int*)malloc(bytes);
 
     //Initialize matrix
     for (int i=0;i<n;i++){
@@ -82,8 +79,9 @@ int main(int argc, char **argv)
     dim3 grid_size(n/block_size.x,n/block_size.y);
     printf("Aqui");
       //<<<Bloques,hilos>>>
-    multiplication  <<<grid_size,block_size>>> (d_a,d_b,d_c,n);
     multiplication2(h_a,h_b,h_c_s,n);
+    multiplication  <<<grid_size,block_size>>> (d_a,d_b,d_c,n);
+  
     //Copy  data  device to host
     printf("hola1");
     cudaMemcpy(h_c,d_c,bytes,cudaMemcpyDeviceToHost);
