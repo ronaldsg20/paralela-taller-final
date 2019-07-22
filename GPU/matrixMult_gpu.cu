a792855b0a28a0c1a5219802ca6b1f6f3806587d
ronaldsg20 <roasarmientoga@unal.edu.co>
2019-07-21 23:13:24 -0500

    again

--- GPU/matrixMult_gpu.cu
+++ GPU/matrixMult_gpu.cu
@@ -80,7 +80,7 @@ __global__ void multiplyMat(int *A,int *B, int *C,int *H,int *N){
             for (j = 0; j < *N; j++) { 
                 C[(i * *N) +j] = 0; 
                 for (k = 0; k < *N; k++)
-                    if(tn==0) printf("multiplying %d  with  %d \n",A[(i * *N) +k],B[(k * *N) +j]);
+                    //if(tn==0) printf("multiplying %d  with  %d \n",A[(i * *N) +k],B[(k * *N) +j]);
                     C[(i * *N) +j] += A[(i * *N) +k]*B[(k * *N) +j]; 
             } 
         }
