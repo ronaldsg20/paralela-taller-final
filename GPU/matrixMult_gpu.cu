ed67c3d33ec9137f23e49cad8daf5b0a3676e0a7
anferoar96 <afromana@unal.edu.co>
2019-07-23 04:34:13 -0500

    Try fix

--- GPU/matrixMult_gpu.cu
+++ GPU/matrixMult_gpu.cu
@@ -9,7 +9,7 @@
  #include <cuda_runtime.h>
 // Global variables
 
-int   N;
+int
 
 void readMatrix(char *filename, int **M, int N){
     FILE *fstream = fopen(filename, "r");
@@ -44,13 +44,13 @@ void printMatrix(int **M, int N){
     printf("\n");
 }
 
-void multiplyMatrix(int A[][N], int B[][N], int C[][N], int ini, int fin) 
+void multiplyMatrix(int A[1024][1024], int B[1024][1024], int C[1024][1024], int ini, int fin) 
 { 
     int i, j, k; 
     for (i = ini; i < fin; i++) { 
-        for (j = 0; j < N; j++) { 
+        for (j = 0; j < 1024; j++) { 
             C[i][j] = 0; 
-            for (k = 0; k < N; k++) 
+            for (k = 0; k < 1024; k++) 
                 C[i][j] += A[i][k]*B[k][j]; 
         } 
     } 
@@ -114,7 +114,7 @@ int main(int argc, char **argv){
     }
     char* fileA = argv[1];
     char* fileB = argv[2];
-    N = atoi(argv[3]);
+    int N = atoi(argv[3]);
     int H = atoi(argv[4]);    
 
     //device variables
